3e8c217f5588138cae1eaec7f81ef76e06ed8af5
Jaime <j-xile@hotmail..com>
2020-02-28 15:07:50 -0800

    Create functions for NBLM, Classification and probabilistic model

--- Programming_2/hw2.py
+++ Programming_2/hw2.py
@@ -14,46 +14,26 @@ def main():
     train, test = initData(infile)
 
     # 2. Create probabalistic model
-    p = [0, 0]
-
-    class1 = np.sum(train[:, NFEATURES], axis=0, dtype=np.float64)
-    p[0] = (train.shape[0] - class1)/train.shape[0]
-    p[1] = class1/train.shape[0]
-
-    μ = np.zeros((NFEATURES, 2))
-    σ = np.zeros((NFEATURES, 2))
-
-    μ[:, 0] = np.mean(train[:907, :NFEATURES], axis=0, dtype=np.float64)
-    μ[:, 1] = np.mean(train[907:, :NFEATURES], axis=0, dtype=np.float64)
-    σ[:, 0] = np.std(train[:907, :NFEATURES], axis=0, dtype=np.float64)
-    σ[:, 1] = np.std(train[907:, :NFEATURES], axis=0, dtype=np.float64)
-
-    σ[:, 0] = np.where(σ[:NFEATURES, 0] == 0, MINSTD, σ[:, 0])
-    σ[:, 1] = np.where(σ[:NFEATURES, 1] == 0, MINSTD, σ[:, 1])
+    p, μ, σ = probmodel(train)
 
     # 3. Run Bayesian learning model
-    tclass = np.zeros((test.shape[0], NFEATURES, 2))
-    aclass = np.ones((test.shape[0], 2))
-    aclass[:, 0] *= math.log2(p[0])
-    aclass[:, 1] *= math.log2(p[1])
+    aclass = nblm(train, p, μ, σ)
+    fclass = classify(test, aclass)
 
-    for j in range(test.shape[0]):
-        for i in range(NFEATURES):
-            tclass[j, i, 0] = ndist(test[j, i], μ[i, 0], σ[i, 0])
-            tclass[j, i, 1] = ndist(test[j, i], μ[i, 1], σ[i, 1])
-            aclass[j, 0] += math.log2(tclass[j, i, 0])
-            aclass[j, 1] += math.log2(tclass[j, i, 1])
+    accuracy = np.sum(fclass) / np.sum(test[:, NFEATURES])
+    print("accuracy = ", accuracy)
 
+    return 0
+
+
+def classify(test, aclass):
     fclass = np.zeros((test.shape[0]))
     for i in range(test.shape[0]):
-        if aclass[j, 0] > aclass[j, 1]:
+        if aclass[i, 0] > aclass[i, 1]:
             fclass[0] = 1
         else:
             fclass[0] = 0
-
-    accuracy = np.sum(fclass) / np.sum(test[:, NFEATURES])
-
-    print("accuracy = ", accuracy)
+    return fclass
 
 
 def example():
@@ -99,8 +79,45 @@ def initData(infile):
     return train, test
 
 
+def nblm(test, p, μ, σ):
+    tclass = np.zeros((test.shape[0], NFEATURES, 2))
+    aclass = np.ones((test.shape[0], 2))
+    aclass[:, 0] *= math.log2(p[0])
+    aclass[:, 1] *= math.log2(p[1])
+
+    for j in range(test.shape[0]):
+        for i in range(NFEATURES):
+            tclass[j, i, 0] = ndist(test[j, i], μ[i, 0], σ[i, 0])
+            tclass[j, i, 1] = ndist(test[j, i], μ[i, 1], σ[i, 1])
+            aclass[j, 0] += math.log2(tclass[j, i, 0])
+            aclass[j, 1] += math.log2(tclass[j, i, 1])
+
+    return aclass
+
+
 def ndist(x, μ, σ):
     return (1 / (math.sqrt(2 * math.pi) * σ)) * (math.exp(-1 * pow(x - μ, 2) / (2 * pow(σ, 2))))
+
+
+def probmodel(train):
+    p = [0, 0]
+
+    class1 = np.sum(train[:, NFEATURES], axis=0, dtype=np.float64)
+    p[0] = (train.shape[0] - class1) / train.shape[0]
+    p[1] = class1 / train.shape[0]
+
+    μ = np.zeros((NFEATURES, 2))
+    σ = np.zeros((NFEATURES, 2))
+
+    μ[:, 0] = np.mean(train[:907, :NFEATURES], axis=0, dtype=np.float64)
+    μ[:, 1] = np.mean(train[907:, :NFEATURES], axis=0, dtype=np.float64)
+    σ[:, 0] = np.std(train[:907, :NFEATURES], axis=0, dtype=np.float64)
+    σ[:, 1] = np.std(train[907:, :NFEATURES], axis=0, dtype=np.float64)
+
+    σ[:, 0] = np.where(σ[:NFEATURES, 0] == 0, MINSTD, σ[:, 0])
+    σ[:, 1] = np.where(σ[:NFEATURES, 1] == 0, MINSTD, σ[:, 1])
+
+    return p, μ, σ
 
 
 if __name__ == '__main__':
