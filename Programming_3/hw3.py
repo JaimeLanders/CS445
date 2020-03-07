0d2d96d4a18a15572f49526ff1c9c765f1e4f143
Jaime <j-xile@hotmail..com>
2020-03-06 21:52:32 -0800

    Fix shallow copy causing algorithm to exit early

--- Programming_3/hw3.py
+++ Programming_3/hw3.py
@@ -1,3 +1,5 @@
+import copy
+
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -24,11 +26,11 @@ def assignment(x, m, k, S):
         for i in range(k):
             #        S[i] = assignment(x, m[i], k)
             t[i] = twonorm(x[p], m[i])
-        #                np.append(S[i], assignment(x[p], m[i], k))
+#                np.append(S[i], assignment(x[p], m[i], k))
         s = np.argmin(t)
-        #            np.append(S[3], (x[p, 0], x[p, 1]), axis=1)
+ #            np.append(S[3], (x[p, 0], x[p, 1]), axis=1)
         xs = (x[p, 0], x[p, 1])
-        #            S = np.append(S, xs )
+ #            S = np.append(S, xs )
         S[s].append(xs)
 
     return S
@@ -41,11 +43,11 @@ def kmeans(x, k, r):
     m = x[idx, :]
 
 #    S = np.empty([k, 1], dtype=tuple)
-    S = []
-    for i in range(k):
-        t = [(m[i, 0], m[i, 1])]
+#    S = []
+#    for i in range(k):
+#        t = [(m[i, 0], m[i, 1])]
 #        S[i, 0] = t
-        S.append(t)
+#        S.append(t)
 
     # 2. repeat until Centroid do not change:
     ml = np.zeros((k))
@@ -54,17 +56,22 @@ def kmeans(x, k, r):
         print(it)
         it += 1
         # Form K clusters by assigning each point to its closest centroid
+        S = []
+        for i in range(k):
+            t = [(m[i, 0], m[i, 1])]
+            S.append(t)
         S = assignment(x, m, k, S)
 
         # Recompute the centroid of each cluster
         m = update(m, S)
+        print()
 
         if np.any(ml != 0.0):
             t = m - ml
-            if max(t.max(), t.min(), key=abs) < 0.01:
+            if max(t.max(), t.min(), key=abs) < 0.001:
                 plot(m, 'mplot')
                 return 0
-        ml = m
+        ml = copy.deepcopy(m)
         print()
 
     print()
