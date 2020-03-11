a3a5602f4bcf431ef081b205d9a26e1a1b01f1d0
Jaime <j-xile@hotmail..com>
2020-03-11 02:26:07 -0700

    Implement FCM on data set

--- Programming_3/hw3.py
+++ Programming_3/hw3.py
@@ -21,86 +21,126 @@ def main():
         k = int(sys.argv[2])
         r = int(sys.argv[3])
 
-        ms = list(range(r))
+        Ms = list(range(r))
         S = list(range(r))
         sse = np.zeros((r))
         for i in range(r):
-            ms[i], S[i], sse[i] = kmeans(data, k)
+            Ms[i], S[i], sse[i] = kmeans(data, k)
 
         lsse = np.argmin(sse)
         print(f'''The minimum SSE for the {r} runs is: {sse[lsse]}''')
-        plot(S[lsse], ms[lsse], 'mplot.png')
+        kplot(S[lsse], Ms[lsse], 'kplot.png')
     elif mode == 2:
-        cmeans(data)
+        # Choose a number of clusters: c (a hyperparameter).
+        c = int(sys.argv[2])
+        m = int(sys.argv[3]) # Fuzzifier
+        r = int(sys.argv[4])
+#        for i in range(r):
+        W, C = cmeans(data, c, m)
+#        print(f'''The minimum SSE for the {r} runs is: {sse[lsse]}''')
+        cplot(data, W, C, 'cplot.png')
     else:
-        print('Usage: python hw3 mode{1 = k-Means, 2 = FCM} K/c r')
+        print('\nUsage: python hw3 mode hyperparam1 (hyperparam2) #runs, (i.e):'
+              '       \nk-Means mode (1): python hw3 1 K r'
+              '       \nFCM mode (2): python hw3 2 c m r')
         return -1
 
     return 0
 
 
-def assignment(x, m, k, S):
-    for p in range(x.shape[0]):
-        t = np.zeros((k))
-        for i in range(k):
-            t[i] = twonorm(x[p], m[i])
-        s = np.argmin(t)
-        xs = (x[p, 0], x[p, 1])
-        S[s].append(xs)
+def cestep(X, W, m, C):
+    for i in range(W.shape[0]):
+        for j in range(W.shape[1]):
+            sum = 0
+            for k in range(C.shape[0]):
+                sum += pow(twonorm(X[i], C[j], 1) /
+                                  (twonorm(X[i], C[k], 1)), (2 / (m - 1)))
 
-    return S
+            W[i, j] = 1 / sum
+    return W
 
 
-def cmeans(x):
-    m = 2
-    x = np.array([[1, 2], [0, -1]])
-    w = np.array([[0.4, 0.6], [0.7, 0.3]])
-
-    # Choose a number of clusters: c (a hyperparameter).
-#    c = sys.argv[2]
+def cmeans(X, c, m):
+    # D2L example
+#    m = 2
+#    x = np.array([[1, 2], [0, -1]])
+#    w = np.array([[0.4, 0.6], [0.7, 0.3]])
+#    c = cmstep(x, w, m)
+#    w = cestep(x, w, m, c)
 
     # Initially assign coefficients randomly to each data point for being in the
     # clusters (these are the initial membership grades).
+#    W = np.random.randint(1, X.shape[0], size = (X.shape[0], c))
+    W = np.random.randint(1, 10, size = (X.shape[0], c))
+    W = W / np.sum(W)
+    C = np.zeros((c, X.shape[1]))
 
+    cl = 0
     # Repeat until the algorithm has converged/stopping condition:
     while True:
         # (I) Compute the centroid for each cluster (m-step).
-        c = cmstep(x, w, m)
+        C = cmstep(X, W, m, c)
 
         # (II) For each data point, compute its coefficients/membership grades
         # for being in the clusters (e-step).
-        w = cestep(x, w, m, c)
+        W = cestep(X, W, m, C)
 
-        break # temp
+        if np.any(cl != 0.0):
+            t = C - cl
+            if max(t.max(), t.min(), key=abs) < MINDELTA:
+                break
+        cl = copy.deepcopy(C)
 
-    print()
-    return 0
-
-
-def cestep(x, w, m, c):
-    for i in range(w.shape[0]):
-        for j in range(w.shape[1]):
-            sum = 0
-            for k in range(c.shape[0]):
-                sum += pow(twonorm(x[i], c[j], 1) /
-                               (twonorm(x[i], c[k], 1)), (2 / (m - 1)))
-
-            w[i, j] = 1 / sum
-    return w
+    return W, C
 
 
-def cmstep(x, w, m):
-    c = np.zeros((2, 2))
-    for k in range(c.shape[0]):
+def cmstep(X, W, m, c):
+    C = np.zeros((c, X.shape[1]))
+    for k in range(C.shape[0]):
         numsum = 0
         densum = 0
-        for i in range(x.shape[0]):
-            numsum += w[i, k] ** m * x[i]
-            densum += w[i, k] ** m
-            print()
-        c[k] = numsum/densum
+        for i in range(X.shape[0]):
+            numsum += W[i, k] ** m * X[i]
+            densum += W[i, k] ** m
+        C[k] = numsum/densum
 
-    return c
+    return C
+
+
+def cplot(X, W, C, fname):
+    fig, ax = plt.subplots()
+
+    clusters = []
+    for i in range(C.shape[0]):
+        clusters.append([(0, 0)])
+
+    for i in range(X.shape[0]):
+#        for j in range(W.shape[0]):
+        xc = np.argmax(W[i])
+        xs = (X[i, 0], X[i, 1])
+#        if clusters[xc] == 0:
+#           clusters[xc][0] = xs[0]
+#           clusters[xc][1] = xs[1]
+#        else:
+        clusters[xc].append(xs)
+
+    for i in range(len(clusters)):
+        Xix, Xiy = zip(*clusters[i])
+        plt.scatter(Xix, Xiy)
+
+    plt.scatter(C[:, 0], C[:, 1], marker='+') # Centroids
+    plt.savefig(fname)
+
+
+def kestep(m, S):
+    for i in range(m.shape[0]):
+        sum = [0, 0]
+        for j in range(1, len(S[i])):
+            sum[0] += S[i][j][0]
+            sum[1] += S[i][j][1]
+        t =  (sum[0] / len(S[i]), sum[1] / len(S[i]))
+        m[i] = t
+    return m
 
 
 def kmeans(x, k):
@@ -118,14 +158,14 @@ def kmeans(x, k):
         for i in range(k):
             t = [(m[i, 0], m[i, 1])]
             S.append(t)
-        S = assignment(x, m, k, S)
+        S = kmstep(x, m, k, S)
 
         # Recompute the centroid of each cluster
-        m = update(m, S)
+        m = kestep(m, S)
 
         if np.any(ml != 0.0):
             t = m - ml
-            if max(t.max(), t.min(), key=abs) < 0.001:
+            if max(t.max(), t.min(), key=abs) < MINDELTA:
                 break
         ml = copy.deepcopy(m)
 
@@ -134,7 +174,19 @@ def kmeans(x, k):
     return m, S, sse
 
 
-def plot(S, m, fname):
+def kmstep (x, m, k, S):
+    for p in range(x.shape[0]):
+        t = np.zeros((k))
+        for i in range(k):
+            t[i] = twonorm(x[p], m[i])
+        s = np.argmin(t)
+        xs = (x[p, 0], x[p, 1])
+        S[s].append(xs)
+
+    return S
+
+
+def kplot(S, m, fname):
     fig, ax = plt.subplots()
     for i in range(len(S)):
         for j in range(len(S[i])):
@@ -157,25 +209,14 @@ def sumofsquares(S):
     return sse
 
 
-def twonorm(x, m, d):
+def twonorm(x, m, p):
     sum = 0
     for i in range(x.size):
         sum += pow(x[i] - m[i], 2)
-    if d == 1:
+    if p == 1:
         return math.sqrt(sum)
     else:
         return sum
-
-
-def update(m, S):
-    for i in range(m.shape[0]):
-        sum = [0, 0]
-        for j in range(1, len(S[i])):
-            sum[0] += S[i][j][0]
-            sum[1] += S[i][j][1]
-        t =  (sum[0] / len(S[i]), sum[1] / len(S[i]))
-        m[i] = t
-    return m
 
 
 if __name__ == '__main__':
